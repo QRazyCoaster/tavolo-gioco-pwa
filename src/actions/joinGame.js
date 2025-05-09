2a7410330fcbb0c9820daa67863d25c0101b690e
QRazyCoaster <deniero@denierob.com>
2025-05-09 15:13:18 +0200

    Update joinGame.js

--- src/actions/joinGame.js
+++ src/actions/joinGame.js
@@ -1,7 +1,12 @@
 import { supabase } from '@/supabaseClient';
-import { listBuzzers } from '@/actions/listBuzzers';
-
-export async function joinGame() {
-  // codice da aggiungere nei passi successivi
-}
-
+import { listBuzzers } from '@/actions/listBuzzers'
+export async function joinGame({ gameId, playerName }) {
+  const { data: player, error } = await supabase
+    .from('players')
+    .insert({
+      game_id: gameId,
+      name: playerName,
+      is_host: false
+    })
+    .single();
+  if (error) throw error;
