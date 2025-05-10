d67a55ad5a083dd0c12c0410be1c5f624d6dbdff
QRazyCoaster <deniero@denierob.com>
2025-05-10 18:15:51 +0200

    Update supabaseClient.ts

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -3,8 +3,8 @@ import { createClient } from '@supabase/supabase-js';
 
 // Supabase project URL and anon key (safe to use in browser)
 // Questi valori sono forniti per il test, in un'app reale andrebbero nelle variabili d'ambiente
-const supabaseUrl = 'https://yhsxqxguqgeexbfmadil.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inloc3hxeGd1cWdlZXhiZm1hZGlsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTYyMjU1MzAsImV4cCI6MjAzMTgwMTUzMH0.RsD0v-h1jPGe-1CCtXQQhgjVu17YC6eK1WSXi9aB1pA';
+const supabaseUrl = 'https://ybjcwjmzwgobxgopntpy.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InliamN3am16d2dvYnhnb3BudHB5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY2OTY4ODAsImV4cCI6MjA2MjI3Mjg4MH0.Wbf__F-WKhCkkQefz-NNoPGjDIGtIluixZbwsDeIfkU';
 
 // Create a single supabase client for interacting with your database
 export const supabase = createClient(supabaseUrl, supabaseKey);
